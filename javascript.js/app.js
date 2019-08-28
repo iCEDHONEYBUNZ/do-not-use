a85782a25c1e5f10c088e5548ebe688c8147918a
iCEDHONEYBUNZ <48493703+iCEDHONEYBUNZ@users.noreply.github.com>
2019-08-27 19:54:09 -0500

    setting changes

--- javascript.js/app.js
+++ javascript.js/app.js
@@ -1,5 +1,3 @@
-const logo = document.querySelectorAll("#logo path");
+const logo = document.querySelectorAll(`#logo path`);
 
-for (let i = 0; i < logo.length; i++) {
-    console.log('letter ${i} is ${logo[i].gettotallength()}');
-}
+console.log(logo);
