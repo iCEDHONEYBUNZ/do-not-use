716eb45b975425ea8d1aab80a1fdb125e4d51fa3
iCEDHONEYBUNZ <48493703+iCEDHONEYBUNZ@users.noreply.github.com>
2020-02-03 15:11:41 -0600

    added a few pokemon and stats to repositry

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,6 +1,33 @@
 var repository = [
-    [Pikachu],
-    [bulbasaur],
-    [charmander],
-    [squirtle]
+    {name:'bulbasaur', height: 0.7, types: ['grass', 'fire'] },
+    {name:'charmander', height: 0.6, types: ['fire'] },
+    {name:'cloyster', height: 1.5, types: ['ice', 'water'] },
 ];
+
+for (let i = 0; i < repository.length; i++) {
+    let pokemon = repository[i];
+    if (pokemon.height > 5){
+        document.write(
+            pokemon.name +
+            ''+
+            ''+
+            'height:' +
+            ''+
+            pokemon.height +
+            '' +
+            ' -' +
+            ''+
+            "wow, thats big!"
+        );
+    } else {
+        document.write(
+            pokemon.name +
+            '' +
+            '' +
+            'height:' +
+            '' +
+            '<br>' +
+            '<br>'
+        );
+    }
+}
