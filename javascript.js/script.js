68cc4e6f024bf910ce1cdd2a2ff56fd64c31e1e7
iCEDHONEYBUNZ <48493703+iCEDHONEYBUNZ@users.noreply.github.com>
2020-02-06 16:28:34 -0600

    added new pokemon to repository

--- javascript.js/script.js
+++ javascript.js/script.js
@@ -1,33 +1,512 @@
 var repository = [
-    {name:'bulbasaur', height: 0.7, types: ['grass', 'fire'] },
-    {name:'charmander', height: 0.6, types: ['fire'] },
-    {name:'cloyster', height: 1.5, types: ['ice', 'water'] }
+    { 
+        ID: 1,
+        name: 'bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison'] 
+    },
+    { 
+        ID: 2,
+        name: 'Ivyasaur',
+        height: 1,
+        type: ['grass', 'poison'] 
+    },
+    { 
+        ID: 3,
+        name: 'venusaur',
+        height: 2,
+        type: ['grass', 'poison'] 
+    },
+    {
+        ID: 4,
+        name: 'charmander',
+        height: 0.6,
+        type: ['fire']
+    },
+    {
+        ID: 5,
+        name: 'charmeleon',
+        height: 1.1,
+        type: ['fire']
+    },
+    {
+        ID: 6,
+        name: 'charizard',
+        height: 1.7,
+        type: ['fire', 'flying']
+    },
+    {
+        ID: 7,
+        name: 'squirtle',
+        height: 0.5,
+        type: ['water']
+    },
+    {
+        ID: 8,
+        name: 'wartortle',
+        height: 1,
+        type: ['water']
+    },
+    {
+        ID: 9,
+        name: 'Blastoise',
+        height: 1.6,
+        type: ['water']
+    },
+    {
+        ID: 10,
+        name: 'caterpie',
+        height: 0.3,
+        type: ['bug'] 
+    },
+    {
+        ID: 11,
+        name: 'metapod',
+        height: 0.7,
+        type: ['bug'] 
+    },
+    {
+        ID: 12,
+        name: 'butterfree',
+        height: 1.1,
+        type: ['bug', 'flying'] 
+    },
+    {
+        ID: 13,
+        name: 'weedle',
+        height: 0.3,
+        type: ['bug', 'poison'] 
+    },
+    {
+        ID: 14,
+        name: 'kakuna',
+        height: 0.6,
+        type: ['bug', 'poison'] 
+    },
+    {
+        ID: 15,
+        name: 'Beedrill',
+        height: 0.3,
+        type: ['bug', 'poison'] 
+    },
+    {
+        ID: 16,
+        name: 'pidgey',
+        height: 0.3,
+        type: ['flying', 'normal'] 
+    },
+    {
+        ID: 17,
+        name: 'pidgeotto',
+        height: 1.1,
+        type: ['flying', 'normal'] 
+    },
+    {
+        ID: 18,
+        name: 'pidgeot',
+        height: 1.5,
+        type: ['flying', 'normal'] 
+    },
+    {
+        ID: 19,
+        name: 'rattata',
+        height: 0.3,
+        type: ['normal'] 
+    },
+    {
+        ID: 20,
+        name: 'ratticate',
+        height: 0.7,
+        type: ['normal'] 
+    },
+    {
+        ID: 21,
+        name: 'spearow',
+        height: 0.3,
+        type: ['flying', 'normal'] 
+    },
+    {
+        ID: 22,
+        name: 'fearow',
+        height: 1.2,
+        type: ['flying', 'normal'] 
+    },
+    {
+        ID: 23,
+        name: 'ekans',
+        height: 2,
+        type: ['poison'] 
+    },
+    {
+        ID: 24,
+        name: 'arbok',
+        height: 3.5,
+        type: ['poison'] 
+    },
+    {
+        ID: 25,
+        name: 'pikachu',
+        height: 0.4,
+        type: ['electric'] 
+    },
+    {
+        ID: 26,
+        name: 'raichu',
+        height: 0.8,
+        type: ['electric'] 
+    },
+    {
+        ID: 27,
+        name: 'sandshrew',
+        height: 0.6,
+        type: ['ground'] 
+    },
+    {
+        ID: 28,
+        name: 'sandslash',
+        height: 1,
+        type: ['ground'] 
+    },
+    {
+        ID: 29,
+        name: 'nidoran',
+        height: 0.4,
+        type: ['poison'] 
+    },
+    {
+        ID: 30,
+        name: 'nidorina',
+        height: 0.8,
+        type: ['poison'] 
+    },
+    {
+        ID: 31,
+        name: 'nidoqueen',
+        height: 1.3,
+        type: ['ground', 'poison'] 
+    },
+    {
+        ID: 32,
+        name: 'nidoran',
+        height: 0.5,
+        type: ['poison'] 
+    },
+    {
+        ID: 33,
+        name: 'nidorino',
+        height: 0.9,
+        type: ['poison'] 
+    },
+    {
+        ID: 34,
+        name: 'nidoking',
+        height: 1.4,
+        type: ['ground', 'poison'] 
+    },
+    {
+        ID: 35,
+        name: 'clefairy',
+        height: 0.6,
+        type: ['fairy'] 
+    },
+    {
+        ID: 36,
+        name: 'clefable',
+        height: 1.3,
+        type: ['fairy'] 
+    },
+    {
+        ID: 37,
+        name: 'vulpix',
+        height: 0.6,
+        type: ['fire'] 
+    },
+    {
+        ID: 38,
+        name: 'ninetales',
+        height: 1.1,
+        type: ['fire'] 
+    },
+    {
+        ID: 39,
+        name: 'jigglypuff',
+        height: 0.5,
+        type: ['fairy', 'normal'] 
+    },
+    {
+        ID: 40,
+        name: 'wigglytuff',
+        height: 1,
+        type: ['fairy', 'normal'] 
+    },
+    {
+        ID: 41,
+        name: 'zubat',
+        height: 0.8,
+        type: ['poison', 'flying'] 
+    },
+    {
+        ID: 41,
+        name: 'zubat',
+        height: 0.8,
+        type: ['poison', 'flying'] 
+    },
+    {
+        ID: 42,
+        name: 'golbat',
+        height: 1.6,
+        type: ['poison', 'flying'] 
+    },
+    {
+        ID: 43,
+        name: 'oddish',
+        height: 0.5,
+        type: ['grass', 'poison'] 
+    },
+    {
+        ID: 44,
+        name: 'gloom',
+        height: 0.8,
+        type: ['grass', 'poison'] 
+    },
+    {
+        ID: 45,
+        name: 'vileplume',
+        height: 0.5,
+        type: ['grass', 'poison'] 
+    },
+    {
+        ID: 46,
+        name: 'Paras',
+        height: 0.3,
+        type: ['grass', 'bug'] 
+    },
+    {
+        ID: 47,
+        name: 'Parasect',
+        height: 1,
+        type: ['grass', 'bug'] 
+    },
+    {
+        ID: 48,
+        name: 'venonat',
+        height: 1,
+        type: ['bug', 'poison'] 
+    },
+    {
+        ID: 49,
+        name: 'venomoth',
+        height: 1.5,
+        type: ['bug', 'poison'] 
+    },
+    {
+        ID: 50,
+        name: 'diglett',
+        height: 0.2,
+        type: ['ground'] 
+    },
+    {
+        ID: 51,
+        name: 'dugtrio',
+        height: 0.7,
+        type: ['ground']
+    },
+    {
+        ID: 52,
+        name: 'meowth',
+        height: 0.4,
+        type: ['normal']
+    },
+    {
+        ID: 53,
+        name: 'persian',
+        height: 1,
+        type: ['normal']
+    },
+    {
+        ID: 54,
+        name: 'psyduck',
+        height: 0.8,
+        type: ['water']
+    },
+    {
+        ID: 55,
+        name: 'golduck',
+        height: 1.7,
+        type: ['water']
+    },
+    {
+        ID: 56,
+        name: 'mankey',
+        height: 0.5,
+        type: ['fighting']
+    },
+    {
+        ID: 57,
+        name: 'primeape',
+        height: 1,
+        type: ['fighting']
+    },
+    {
+        ID: 58,
+        name: 'growlithe',
+        height: 0.7,
+        type: ['fire']
+    },
+    {
+        ID: 59,
+        name: 'arcanine',
+        height: 1.9,
+        type: ['fire']
+    },
+    {
+        ID: 60,
+        name: 'poliwag',
+        height: 0.6,
+        type: ['water']
+    },
+    {
+        ID: 61,
+        name: 'poliwhirl',
+        height: 1,
+        type: ['water']
+    },
+    {
+        ID: 62,
+        name: 'poliwrath',
+        height: 1.3,
+        type: ['water', 'fighting']
+    },
+    {
+        ID: 63,
+        name: 'abra',
+        height: 0.9,
+        type: ['psychic']
+    },
+    {
+        ID: 64,
+        name: 'kadabra',
+        height: 1.3,
+        type: ['psychic']
+    },
+    {
+        ID: 65,
+        name: 'alakazam',
+        height: 1.5,
+        type: ['psychic']
+    },
+    {
+        ID: 66,
+        name: 'machop',
+        height: 0.8,
+        type: ['fighting']
+    },
+    {
+        ID: 67,
+        name: 'machoke',
+        height: 0.8,
+        type: ['fighting']
+    },
+    {
+        ID: 68,
+        name: 'machamp',
+        height: 1.6,
+        type: ['fighting']
+    },
+    {
+        ID: 69,
+        name: 'bellsprout',
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        ID: 70,
+        name: 'weepinbell',
+        height: 1,
+        type: ['grass', 'poison']
+    },
+    {
+        ID: 71,
+        name: 'victreebel',
+        height: 1.7,
+        type: ['grass', 'poison']
+    },
+    {
+        ID: 72,
+        name: 'tentacool',
+        height: 0.9,
+        type: ['water', 'poison']
+    },
+    {
+        ID: 73,
+        name: 'tentacruel',
+        height: 1.6,
+        type: ['water', 'poison']
+    },
+    {
+        ID: 74,
+        name: 'geodude',
+        height: 0.4,
+        type: ['rock', 'ground']
+    },
+    {
+        ID: 75,
+        name: 'graveler',
+        height: 1,
+        type: ['rock', 'ground']
+    },
+    {
+        ID: 76,
+        name: 'golem',
+        height: 1.4,
+        type: ['rock', 'ground']
+    },
+    {
+        ID: 77,
+        name: 'ponyta',
+        height: 1,
+        type: ['fire']
+    },
+    {
+        ID: 78,
+        name: 'rapidash',
+        height: 1.7,
+        type: ['fire']
+    },
+    {
+        ID: 79,
+        name: 'slowpoke',
+        height: 1.2,
+        type: ['psychic', 'water']
+    },
+    {
+        ID: 80,
+        name: 'slowbro',
+        height: 1.6,
+        type: ['psychic', 'water']
+    },
+    {
+        ID: 81,
+        name: 'magnemite',
+        height: 0.3,
+        type: ['electric', 'steel']
+    },
+    {
+        ID: 82,
+        name: 'magneton',
+        height: 1,
+        type: ['electric', 'steel']
+    },
+    {
+        ID: 83,
+        name: 'farfetchd',
+        height: 0.8,
+        type: ['flying', 'normal']
+    },
+    {
+        ID: 84,
+        name: 'doduo',
+        height: 1.4,
+        type: ['flying', 'normal']
+    },
 ];
-
-for (let i = 0; i < repository.length; i++) {
-    let pokemon = repository[i];
-    if (pokemon.height > 5){
-        document.write(
-            pokemon.name +
-            ''+
-            ''+
-            'height:' +
-            ''+
-            pokemon.height +
-            '' +
-            ' -' +
-            ''+
-            "wow, thats big!"
-        );
-    } else {
-        document.write(
-            pokemon.name +
-            '' +
-            '' +
-            'height:' +
-            '' +
-            '<br>' +
-            '<br>'
-        );
-    }
-}
